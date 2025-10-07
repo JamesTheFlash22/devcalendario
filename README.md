2133fcc9a58beb45c1efb8747b6aa6d433196fab
JamesTheFlash22 <davidebu43@gmail.com>
2025-10-07 18:38:45 +0200

    Primo commit: aggiunta progetto Flutter

---
+++ README.md
@@ -0,0 +1,21 @@
+# Webview_Template
+
+![image](https://user-images.githubusercontent.com/68809278/197468633-f0e90392-e8a3-4b66-b200-53939448236b.png)
+
+Easily convert a website into an app with this template.
+Its simple and fun
+
+> Getting Started
+
+* Clone the repository to your desktop.
+* Open lib/main.dart from the folder Flutter-WebviewTemplate
+* replace the link with your desired link destination.
+* Build the app using the command- <br> <br> 
+```  
+ flutter build apk 
+ 
+```
+And thats it You have created your own easy webview app!!!!
+
+
+If you dont have flutter [Click here](https://flutter.dev/) to install.
---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calendario della Classe',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Calendario 1BI'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late WebViewController _controller;
+
+Future<bool> _onWillPop(BuildContext context) async {
+    if (await _controller.canGoBack()) {
+      _controller.goBack();
+      return Future.value(false);
+    } else {
+      return Future.value(true);
+    }
+  }
+
+  _goBack() async{
+    if (await _controller.canGoBack()) {
+      await _controller.goBack();
+    }
+  }
+  _goForward() async {  
+    if (await _controller.canGoForward()) {
+      await _controller.goForward();
+    }
+  }
+
+  
+  @override
+
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      
+      onWillPop: () => _onWillPop(context),
+       
+      child: Scaffold(
+        body: SafeArea(
+          child: WebView(
+          // Replace the below link with the link that you desire!
+          initialUrl: "https://flutter.dev/",
+          javascriptMode: JavascriptMode.unrestricted,
+          
+          onWebViewCreated: (WebViewController webviewcontroller){
+            _controller = webviewcontroller;
+          },
+        ),
+        ),
+      ),
+    );
+  }
+}
